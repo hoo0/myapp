04947f1de76f24b19fb383ede86fb20bc041f1c4
FlutLab.io <>
2022-06-14 23:50:47 +0000

    Autosave

--- lib/screens/schedule2_section_screen.dart
+++ lib/screens/schedule2_section_screen.dart
@@ -88,14 +88,22 @@ class ScheduleSectionScreen extends StatelessWidget {
                   decoration: BoxDecoration(),
                   child: Align(
                     alignment: AlignmentDirectional(0, 0),
-                    child: Text(
-                      '천안아산',
-                      style: FlutterFlowTheme.of(context).bodyText1.copyWith(
+                    child: TextFormField(
+                      // controller: textController,
+                      // onChanged: () {},
+                      autofocus: true,
+                      obscureText: false,
+                      decoration: InputDecoration(
+                        enabledBorder: InputBorder.none,
+                        focusedBorder: InputBorder.none,
+                      ),
+                      style: FlutterFlowTheme.of(context).bodyText1.override(
                             fontFamily: 'Poppins',
                             color: Color(0xFF004676),
                             fontSize: 30,
                             fontWeight: FontWeight.w600,
                           ),
+                      textAlign: TextAlign.center,
                     ),
                   ),
                 ),

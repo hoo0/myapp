2446b5cc868eb10c537ea6b75e262347ca3c6f9f
FlutLab.io <>
2022-06-15 11:46:05 +0000

    Autosave

--- lib/screens/schedule2_calendarsub_screen.dart
+++ lib/screens/schedule2_calendarsub_screen.dart
@@ -46,7 +46,7 @@ class ScheduleCalendarsubScreen extends StatelessWidget {
                       Align(
                         alignment: AlignmentDirectional(0, 1),
                         child: Text(
-                          '11',
+                          '출발일',
                           textAlign: TextAlign.center,
                           style: FlutterFlowTheme.of(context).bodyText1.copyWith(
                                 fontFamily: 'Roboto',
